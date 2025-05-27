d6f223c8d00a6ccd423ad1bc19cf974f11131b5a
DanielAlxzBrand <daniel_ale.brand@uao.edu.co>
2025-05-27 14:59:26 -0500

    Implementando metodos def para clase participante

--- app/logica.py
+++ app/logica.py
@@ -18,3 +18,26 @@ class Participante:
         self.clases_asistidas = clases_asistidas  
         self.asistencias = []  
         self.pagos = []        
+            
+    def registrar_asistencia(self, fecha):
+        self.asistencias.append(fecha)
+        self.clases_asistidas += 1
+
+    def registrar_pago(self, monto):
+        self.pagos.append(monto)
+
+    def calcular_pago_mes(self):
+        
+        tarifa = self.TARIFAS.get(self.taller, 0)
+        return tarifa * self.clases_asistidas
+
+    def total_pagado(self):
+        return sum(self.pagos)
+
+    def total_asistencias(self):
+        return len(self.asistencias)
+
+    def datos_incompletos(self):
+        
+        campos = [self.nombre, self.apellido, self.edad, self.taller, self.telefono, self.email, self.mes]
+        return any(c is None or c == "" for c in campos)
