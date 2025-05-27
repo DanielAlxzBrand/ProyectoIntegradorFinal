84eb7c9a44063b61a902facbfe14f44f5c2cdd58
santbce <santiago_jos.becerra@uao.edu.co>
2025-05-27 18:53:36 -0500

    segundo avance participantes

--- app/analisis.py
+++ app/analisis.py
@@ -13,3 +13,13 @@ class Analisis:
         campos_obligatorios = ['nombre', 'apellido', 'edad', 'taller', 'telefono', 'email', 'mes']
         incompletos = self.df[self.df[campos_obligatorios].isnull().any(axis=1) | (self.df[campos_obligatorios] == '').any(axis=1)]
         return incompletos
+    
+    def promedio_pago_por_taller(self):
+        return self.df.groupby('taller')['valor_pagado'].mean()
+
+    def taller_mas_participantes(self):
+        return self.df['taller'].value_counts().idxmax()
+
+    def participante_mayor_pago(self):
+        idx = self.df['valor_pagado'].idxmax()
+        return self.df.loc[idx]
