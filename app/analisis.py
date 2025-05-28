749b57c2f323330340f05db8841abfe86eeed688
santbce <santiago_jos.becerra@uao.edu.co>
2025-05-27 23:37:20 -0500

    Documentación de analisis.py

--- app/analisis.py
+++ app/analisis.py
@@ -1,20 +1,20 @@
-import pandas as pd
-import matplotlib.pyplot as plt
+import pandas as pd  #Importamos pandas para manejar los datos
+import matplotlib.pyplot as plt  #Importamos matplotlib para graficar los datos
 
 class Analisis:
-    def __init__(self, ruta_datos):
+    def __init__(self, ruta_datos):  #Guardamos la ruta del archivo y cargamos el dataframe
         self.ruta_datos = ruta_datos
         self.df = pd.read_csv(ruta_datos)
 
-    def total_participantes(self):
+    def total_participantes(self): #Contamos cuantos participantes hay en total
         return len(self.df)
 
-    def participantes_incompletos(self):
+    def participantes_incompletos(self): #Buscamos los participantes que tienen algun dato obligatorio vacio o nulo
         campos_obligatorios = ['nombre', 'apellido', 'edad', 'taller', 'telefono', 'email', 'mes']
         incompletos = self.df[self.df[campos_obligatorios].isnull().any(axis=1) | (self.df[campos_obligatorios] == '').any(axis=1)]
         return incompletos
     
-    def promedio_pago_por_taller(self):
+    def promedio_pago_por_taller(self): #Calculamos el promedio de pago por taller
         return self.df.groupby('taller')['valor_pagado'].mean()
 
     def taller_mas_participantes(self):
