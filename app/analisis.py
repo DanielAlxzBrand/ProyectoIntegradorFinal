92da1014e4da439e40e207ffd99de3f0daeda342
santbce <santiago_jos.becerra@uao.edu.co>
2025-05-27 23:43:16 -0500

    Finalizo documentación de analisis.py

--- app/analisis.py
+++ app/analisis.py
@@ -17,14 +17,14 @@ class Analisis:
     def promedio_pago_por_taller(self): #Calculamos el promedio de pago por taller
         return self.df.groupby('taller')['valor_pagado'].mean()
 
-    def taller_mas_participantes(self):
+    def taller_mas_participantes(self): #Vemos cual es el taller con mas participantes
         return self.df['taller'].value_counts().idxmax()
 
-    def participante_mayor_pago(self):
+    def participante_mayor_pago(self): #Vemos cual es el participante que mas ha pagado
         idx = self.df['valor_pagado'].idxmax()
         return self.df.loc[idx]
     
-    def grafico_barras_participantes_por_taller(self):
+    def grafico_barras_participantes_por_taller(self): #Hacemos un grafico de barras para ver la cantidad de participantes por taller
         conteo = self.df['taller'].value_counts()
         conteo.plot(kind='bar', color='skyblue')
         plt.title('Participantes por Taller')
@@ -33,7 +33,7 @@ class Analisis:
         plt.tight_layout()
         plt.show()
 
-    def histograma_edades(self):
+    def histograma_edades(self): #Mostramos un histograma para ver las edades
         self.df['edad'].dropna().astype(int).plot(kind='hist', bins=10, color='lightgreen')
         plt.title('Histograma de Edades')
         plt.xlabel('Edad')
@@ -41,7 +41,7 @@ class Analisis:
         plt.tight_layout()
         plt.show()
 
-    def grafico_circular_talleres(self):
+    def grafico_circular_talleres(self): #hacemos un grafico de torta (circular) para ver la distribucion de participantes por taller
         conteo = self.df['taller'].value_counts()
         conteo.plot(kind='pie', autopct='%1.1f%%', startangle=90)
         plt.title('Distribución de Participantes por Taller')
@@ -49,5 +49,5 @@ class Analisis:
         plt.tight_layout()
         plt.show()
 
-    def guardar_datos(self):
+    def guardar_datos(self): #guardamos los datos actualizados en el archivo CSV
         self.df.to_csv(self.ruta_datos, index=False)
