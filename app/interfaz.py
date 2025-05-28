1d914a1a1489637b3f06f3312c8dfc543cc0c21c
santbce <santiago_jos.becerra@uao.edu.co>
2025-05-28 00:35:21 -0500

    Corrección linea de codigo en interfaz.py

--- app/interfaz.py
+++ app/interfaz.py
@@ -22,7 +22,7 @@ class InterfazApp:
             entry.grid(row=idx, column=1)
             self.entradas[campo] = entry
 
-        #Realizazdo mostrar todos y reportes y eliminar
+        #botones
         tk.Button(self.frame, text="Registrar", command=self.registrar).grid(row=0, column=2, padx=5)
         tk.Button(self.frame, text="Modificar", command=self.modificar).grid(row=1, column=2, padx=5)
         tk.Button(self.frame, text="Eliminar", command=self.eliminar).grid(row=2, column=2, padx=5)
@@ -35,7 +35,7 @@ class InterfazApp:
             self.tabla.heading(campo, text=campo.capitalize())
         self.tabla.pack(padx=10, pady=10, fill="x")
 
-        
+        self.cargar_datos()
 
     def cargar_datos(self):
         try:
@@ -88,6 +88,7 @@ class InterfazApp:
         self.df.to_csv(RUTA_DATOS, index=False)
         self.actualizar_tabla()
         messagebox.showinfo("Éxito", "Participante modificado correctamente.")
+
     def eliminar(self):
         seleccionado = self.tabla.selection()
         if not seleccionado:
@@ -123,10 +124,6 @@ class InterfazApp:
         analisis.grafico_barras_participantes_por_taller()
         analisis.histograma_edades()
         analisis.grafico_circular_talleres()
-
-        
-
-    
 
 if __name__ == "__main__":
     root = tk.Tk()
